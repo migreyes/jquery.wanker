2bd8b4d8b449cec3f6d38190cd0bba8ed7c4157f
migreyes <mig@37signals.com>
2014-02-02 17:16:17 -0600

    Add ability to set a delay before showing message.

--- jquery.wanker.coffee
+++ jquery.wanker.coffee
@@ -1,27 +1,57 @@
+#
+# Wanker 1.0.0
+# http://mig.io/makes/wanker
+#
+# The web was meant to be read, not squished.
+# Display a message on excessive browser resizing.
+#
+# MIT License
+#
+# by Mig Reyes, Designer at Basecamp
+# http://twitter.com/migreyes
+#
+
 (($) ->
   $.fn.wanker = (options) ->
+
     settings = $.extend(
+      delay: 1000
       duration: 1200
     , options)
 
     return @each ()->
       $message = $(@)
-
-      # Restrict this to desktop browsers.
       mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-      resizeTimer = undefined
 
-      $(window).resize ->
-        unless mobile
-          # Reveal the message.
-          $message.fadeIn()
+      unless mobile
+        start = null
+        fired = 0
+        elapsed = null
+        timer = undefined
 
-          # Reset timers.
-          clearTimeout resizeTimer if resizeTimer
+        reset = ->
+          fired = 0
+          elapsed = null
 
-          # Start a new timer.
-          resizeTimer = setTimeout(->
-            resizeTimer = null
+        $(window).resize ->
+          if fired < 1
+            start = new Date()
+            fired++
+          else
+            elapsed = Math.abs(new Date() - start)
+            fired++
+
+          # Reveal the message after the delay is surpassed.
+          if elapsed > settings.delay then $message.fadeIn()
+
+          # Countdown timer before closing and resetting.
+          clearTimeout timer if timer
+          timer = setTimeout(->
             $message.fadeOut()
+            reset()
           , settings.duration)
 ) jQuery
+
+
+
+
--- jquery.wanker.js
+++ jquery.wanker.js
@@ -1,28 +1,60 @@
-(function($) {
-  return $.fn.wanker = function(options) {
-    var settings = $.extend({
-      duration: 1200
-    }, options);
+/*!
+  * Wanker 1.0.0
+  * http://mig.io/makes/wanker
+  *
+  * The web was meant to be read, not squished.
+  * Display a message on excessive browser resizing.
+  *
+  * MIT License
+  *
+  * by Mig Reyes, Designer at Basecamp
+  * http://twitter.com/migreyes
+  *
+  */
 
-    return this.each(function() {
-      var $message = $(this);
-      var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-      var resizeTimer = void 0;
-
-      return $(window).resize(function() {
+(function() {
+  (function($) {
+    return $.fn.wanker = function(options) {
+      var settings;
+      settings = $.extend({
+        delay: 1000,
+        duration: 1200
+      }, options);
+      return this.each(function() {
+        var $message, elapsed, fired, mobile, reset, start, timer;
+        $message = $(this);
+        mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
         if (!mobile) {
-          $message.fadeIn();
-
-          if (resizeTimer) {
-            clearTimeout(resizeTimer);
-          }
-
-          return resizeTimer = setTimeout(function() {
-            resizeTimer = null;
-            return $message.fadeOut();
-          }, settings.duration);
+          start = null;
+          fired = 0;
+          elapsed = null;
+          timer = void 0;
+          reset = function() {
+            fired = 0;
+            return elapsed = null;
+          };
+          return $(window).resize(function() {
+            if (fired < 1) {
+              start = new Date();
+              fired++;
+            } else {
+              elapsed = Math.abs(new Date() - start);
+              fired++;
+            }
+            if (elapsed > settings.delay) {
+              $message.fadeIn();
+            }
+            if (timer) {
+              clearTimeout(timer);
+            }
+            return timer = setTimeout(function() {
+              $message.fadeOut();
+              return reset();
+            }, settings.duration);
+          });
         }
       });
-    });
-  };
-})(jQuery);
+    };
+  })(jQuery);
+
+}).call(this);
